d7a9305bdf656589e9bebd36f484be4435368385
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-05-22 09:12:08 +0900

    Update Main.php

--- src/Lovetwice1012/syogo/Main.php
+++ src/Lovetwice1012/syogo/Main.php
@@ -18,51 +18,37 @@ class Main extends PluginBase implements Listener{
 		       
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-       
 		$this->myConfig = new Config($this->getDataFolder() . "MyConfig.yml", Config::YAML);
-
 	}
 
 	public function onJoin(PlayerJoinEvent $event){
 		$config = $this->myConfig;
-  $player = $event->getPlayer();
-  if($config->exists($player->getName())){
-	  $player->setNameTag($config->get($player->getName()));
-	  $player->setDisplayName($config->get($player->getName()));
-  }		
+  		$player = $event->getPlayer();
+  		if($config->exists($player->getName())){
+	 		$player->setNameTag($config->get($player->getName()));
+	 		$player->setDisplayName($config->get($player->getName()));
+ 		}		
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
 	{
 		$config = $this->myConfig;
         if ($label === "syo") {
-            
 		if(isset($args[0])){
-		if(isset($args[1])){    
 			$player = $this->getServer()->getPlayer($sender->getName());
- 
-
-                    $tag = $args[1]; 
-		    $config->set($player->getName(), "[".$tag."§r]".$player->getName());
-		    $config->save();
-		    $player->setNameTag("[".$tag."§r]".$player->getName());
-		    $player->setDisplayName("[".$tag."§r]".$player->getName());
+                	$tag = $args[1]; 
+			$config->set($player->getName(), "[".$tag."§r]".$player->getName());
+			$config->save();
+		 	$player->setNameTag("[".$tag."§r]".$player->getName());
+		    	$player->setDisplayName("[".$tag."§r]".$player->getName());
 		}else{
 			$player = $this->getServer()->getPlayer($sender->getName());
 			$config->set($player->getName(),$player->getName());
 			$config->save();
-               
-                $player->setNameTag($player->getName());
-                $player->setDisplayName($player->getName());
+                	$player->setNameTag($player->getName());
+                	$player->setDisplayName($player->getName());
 		}
-	    	    $sender->sendMessage("称号が".$config->get($player->getName())."になりました");
-		}else{
-	
-		    $sender->sendMessage("§c使用方法:/syo 称号");
-			}
-		
-            
-	
+	    	$sender->sendMessage("称号が".$config->get($player->getName())."になりました");
         }
         return true;
     }
